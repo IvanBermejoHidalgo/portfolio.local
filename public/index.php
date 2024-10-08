57a1d6007b4f75affeff07b1fecf27a44d2792f4
IvanBermejoHidalgo <ibermejo@elpuig.xeill.net>
2024-10-08 22:39:47 +0200

    prueba

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once "../vendor/autoload.php";
+require_once "../src/controller/ProjectController.php";
+
+// prepare twig
+$loader = new \Twig\Loader\FilesystemLoader('templates');
+$twig = new \Twig\Environment($loader, [
+	'cache' => 'cache',
+	'auto_reload' => true,
+]);
+  
+// $pc = new ProjectController();
+// print_r("<pre>");
+// print_r($pc->getProjects());
+// print_r("</pre>");
+
+// Este array lo tiene que devolver el controlador
+echo $twig->render('index.html', [ ]);
+
+?>
